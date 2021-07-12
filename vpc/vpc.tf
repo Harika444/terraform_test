ae40a68325d43d6dc83b53a49ca91d38123d2cc2
Harika444 <47811739+Harika444@users.noreply.github.com>
2021-07-12 16:58:20 +0530

    Update vpc.tf

--- vpc/vpc.tf
+++ vpc/vpc.tf
@@ -10,10 +10,6 @@ resource "aws_vpc" "prod-vpc" {
     enable_dns_hostnames = "true"
     enable_classiclink = "false"
     instance_tenancy = "default"
-
-    tags {
-        Name = "prod-vpc"
-    }
 }
 
 resource "aws_subnet" "prod-subnet-public-1" {
@@ -21,10 +17,6 @@ resource "aws_subnet" "prod-subnet-public-1" {
     cidr_block = "10.0.1.0/24"
     map_public_ip_on_launch = "true" //it makes this a public subnet
     availability_zone = "eu-west-2a"
-
-    tags {
-        Name = "prod-subnet-public-1"
-    }
 }
 
 output "vpc_id" {
