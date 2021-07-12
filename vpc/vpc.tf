9fef7358eab1bed81ca68ef7de63fba451241a19
Harika444 <47811739+Harika444@users.noreply.github.com>
2021-07-12 17:02:01 +0530

    Update vpc.tf

--- vpc/vpc.tf
+++ vpc/vpc.tf
@@ -20,9 +20,9 @@ resource "aws_subnet" "prod-subnet-public-1" {
 }
 
 output "vpc_id" {
-    value = aws_vpc.prod-vpc.vpc_id
+    value = "${aws_vpc.prod-vpc.id}"
 }
 
 output "subnet_id" {
-    value = aws_subnet.prod-subnet-public-1.subnet_id
+    value = "${aws_subnet.prod-subnet-public-1.id}"
 }
